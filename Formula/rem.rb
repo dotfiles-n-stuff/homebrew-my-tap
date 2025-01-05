784f4b7c878715a4ef111cfe964d1149c9113839
Vladimir Zykov <vova@home>
2025-01-05 20:57:44 +0300

    move rem to base repo

--- Formula/rem.rb
+++ Formula/rem.rb
@@ -1,7 +1,7 @@
 class Rem < Formula
   desc "Удаляет выбранные файлы, отправляя их в корзину macOS."
-  homepage "https://github.com/9th8/rem"
-  url "https://raw.githubusercontent.com/9th8/rem/refs/heads/master/rem"
+  homepage "https://github.com/9th8/homebrew-my-tap/tree/master/rem"
+  url "https://raw.githubusercontent.com/9th8/homebrew-my-tap/refs/heads/master/rem/rem"
   version "1.0.0"
 
   def install
