d716694e336108613f823119def21489898fa6d5
Vladimir Zykov <vova@home>
2025-01-05 20:59:02 +0300

    bump rem version

--- Formula/rem.rb
+++ Formula/rem.rb
@@ -2,7 +2,7 @@ class Rem < Formula
   desc "Удаляет выбранные файлы, отправляя их в корзину macOS."
   homepage "https://github.com/9th8/homebrew-my-tap/tree/master/rem"
   url "https://raw.githubusercontent.com/9th8/homebrew-my-tap/refs/heads/master/rem/rem"
-  version "1.0.0"
+  version "1.0.1"
 
   def install
     bin.install "rem" => "rem"
